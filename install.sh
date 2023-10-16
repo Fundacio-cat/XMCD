ce9b601cb54afe2eff7c405b0f71967c9632a736
Pau <paumartinsecre@gmail.com>
2023-10-16 15:56:20 +0200

    Fitxer d'instal·lació de paquets

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install python3 
+sudo apt-get install firefox
+sudo apt-get install pip
+
+pip install selenium
+pip install --upgrade setuptools wheel
+pip install bs4
+pip install requests
+pip install psycopg2-binary
