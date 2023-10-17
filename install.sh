b4c479ac8a20168c3815e2c9938b7020d54fb239
Pau <paumartinsecre@gmail.com>
2023-10-17 12:27:16 +0200

    Afegit mòdul per a l'execució del Cron

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ sudo apt-get update
 sudo apt-get install python3 
 sudo apt-get install firefox
 sudo apt-get install pip
+sudo apt-get install chromium-chromedriver
+sudo apt-get install xvfb # Per al cron
 
 pip install selenium
 pip install --upgrade setuptools wheel
