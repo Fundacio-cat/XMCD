8a97241f66d5ef346e189b919e74f30b39e3a727
Pau <paumartinsecre@gmail.com>
2023-10-24 10:13:53 +0200

    Codi auditoria funcional

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-sudo apt-get update
-sudo apt-get install python3 
-sudo apt-get install firefox
-sudo apt-get install pip
-sudo apt-get install chromium-chromedriver
-sudo apt-get install xvfb # Per al cron
+sudo apt update
+sudo apt install python3 
+sudo apt install firefox-esr
+sudo apt install pip
+sudo apt install chromium-chromedriver
+sudo apt install xvfb # Per al cron
 
 pip install selenium
 pip install --upgrade setuptools wheel
