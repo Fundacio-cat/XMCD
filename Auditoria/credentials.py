63dcd80debb73bfa64cd651f34b034cff8d6001d
Pau <paumartinsecre@gmail.com>
2023-10-27 11:01:54 +0200

    Afegida codi d'Auditoria del Roman

--- Auditoria/credentials.py
+++
@@ -1,9 +0,0 @@
-
-def obtenir_credentials():
-    host = '10.66.66.1'
-    port = '51432'
-    database = 'catalanet'
-    user = 'catalanet'
-    password = 'NY2ULkiTwc3YRw~^`N~^KXhP#b-i@sJEFHjqyu`&-aU-qZxt$v%JE%~xc_`3dqRp'
-
-    return host, port, database, user, password
