150eb81a5bfce45b9fd3fa89aad87ca14a42b62f
Pau <paumartinsecre@gmail.com>
2023-10-27 16:17:12 +0200

    Afegit el fitxer d'instal·lació a executar un cop configurat un nou sensor des de zero

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt install python3 
+sudo apt install firefox-esr
+sudo apt install pip
+sudo apt install chromium-chromedriver
+sudo apt install xvfb # Per al cron
+
+pip install selenium
+pip install --upgrade setuptools wheel
+pip install bs4
+pip install requests
+pip install psycopg2-binary
+
+chmod +x /home/catalanet/XMCD/Controladors/*
