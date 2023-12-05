22fc193fbd4ce5e20f504b04978ede2b8ceb129f
Pau Martín <pmartin@fundacio.cat>
2023-12-05 12:02:56 +0100

    0.3. Arreglat el funcionament amb Google Chrome.

--- navegadors/navegador_chrome.py
+++ navegadors/navegador_chrome.py
@@ -19,12 +19,14 @@ class ChromeNavegador(NavegadorBase):
         if user_agent:
             options = Options()
             options.add_argument(f"user-agent={user_agent}")
-            options.add_argument("--headless")
-            # options.add_argument("--no-sandbox")
-            # options.add_argument("--disable-dev-shm-usage")
-            # A vegades necessari per a entorns headless
-            # options.add_argument("--disable-gpu")
-            # options.add_argument("--remote-debugging-port=9222")
+            # NO FUNCIONA:
+            #options.add_argument("--lang=en")
+            #options.add_argument("--headless")
+            # NO S'HA PROVAT REALMENT
+            #options.add_argument("--no-sandbox")
+            #options.add_argument("--disable-dev-shm-usage")
+            #options.add_argument("--disable-gpu")
+            #options.add_argument("--remote-debugging-port=9222")
 
             # posar driver_path = config.current_directory+"/Controladors/" + config.CHROME_DRIVER_PATH
             driver_path = os.path.join(
