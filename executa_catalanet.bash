fcc4cfc8332fefdd712cc3c7aa95392302349046
Pau Martín <pmartin@fundacio.cat>
2024-04-26 12:28:54 +0200

    Versió 1.0 en pre-producció

--- executa_catalanet.bash
+++ executa_catalanet.bash
@@ -8,11 +8,11 @@ home_catalanet_monitor=/home/catalanet/XMCD
 arxiuregistre=$home_catalanet_monitor/logs/monitor.log
 echo "Iniciant el procés catalanet amb logs a $arxiuregistre"
 
-# Calcula un retard aleatori entre 0 i 59 minuts
+# Calcula un retard aleatori entre 0 i 14 minuts
 # 1 execució per hora
-retard=$(($RANDOM%(59-1)+1))
+retard=$(($RANDOM%(14-1)+1))
 #echo "Iniciant el procés després d'un retard de $retard minuts"
-#sleep ${retard}m
+sleep ${retard}m
 
 # Executa l'script en un subshell, canvia al directori i executa el comandament,
 # redireccionant qualsevol sortida a /dev/null (descarta la sortida)
--- readme.md
+++ readme.md
@@ -1,1 +1,35 @@
-# Presentació del GitHub pendent...
+# Xarxa de monitoratge del català digital
+
+## Fundació .cat
+## Versió 1.0 posada en marxa el 29-04-24
+
+
+Cada sensor té una password i un usuari personalitzat. 
+
+
+### Crontab de cada sensor
+
+```bash
+0 9-21 * * 1-5 bash /home/catalanet/XMCD/executa_catalanet.bash
+0 1 * * * sudo /sbin/shutdown -r # Reinicia a la 1 de la matinada cada dia
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
